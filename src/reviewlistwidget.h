ffd80da36fcb342b24abdb780b35d7ec53152952
creeper <creeperhjt@sjtu.edu.cn>
2024-01-17 14:01:13 +0800

    update

--- src/reviewlistwidget.h
+++ src/reviewlistwidget.h
@@ -8,7 +8,8 @@ class ReviewListWidget: public AbstractListWidget
     Q_OBJECT
 public:
     explicit ReviewListWidget(QWidget *parent = 0);
-private slots:
+protected:
+    void resizeEvent(QResizeEvent *e);
 };
 
 #endif // REVIEWLISTWIDGET_H
